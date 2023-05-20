320294c4d63fec11282594f9cdfd830ba5c1bddb
krso1002 <56928680+krso1002@users.noreply.github.com>
2023-05-20 15:27:32 +0200

    diverse

--- Python/lesHoyde.py
+++ Python/lesHoyde.py
@@ -1,5 +1,6 @@
 import serial 
 import argparse
+import datetime
 
 maalingerInn = []
 maalingerUt = []
@@ -9,16 +10,19 @@ counterUt = 0
 # Mål høyde og skriv til fil
 def maalHoyde(name):
     global counterInn, counterUt
+    # Inn og ut er for å skille mellom poser som går inn og ut av systemet
     inn = "Inn"
     ut = "Ut"
+    # PoseI og PoseU er for å se når posene ikke lenger er under målerene
     poseInn = "PoseI"
     poseUt = "PoseU"
+    dato = datetime.datetime.now().date()
 
     try:
         serial_port = serial.Serial(name,9600)
         print(f"The Port name is {serial_port.name}")
-        f = open("dataInn.csv", "a")
-        g = open("dataUt.csv", "a")
+        filInn = open(f"{dato}Inn.csv", "w")
+        filUt = open(f"{dato}Ut.csv", "w")
         while True:
             hoyde = serial_port.readline()
             hoydeNy = hoyde.decode("utf-8")
@@ -34,7 +38,7 @@ def maalHoyde(name):
             if hoydeNy.find(poseInn) != -1 and len(maalingerInn) > 0:
                 counterInn += 1
                 gjennomsnittInn = regnGjennomsnitt(maalingerInn)
-                f.write(f"{counterInn}, {gjennomsnittInn}, {len(maalingerInn)}\n ")
+                filInn.write(f"{counterInn}, {gjennomsnittInn}, {len(maalingerInn)}\n ")
                 maalingerInn.clear()
             
             # ser etter poser som går ut
@@ -48,24 +52,40 @@ def maalHoyde(name):
             if hoydeNy.find(poseUt) != -1 and len(maalingerUt) > 0:
                 counterUt += 1
                 gjennomsnittUt = regnGjennomsnitt(maalingerUt)
-                g.write(f"{counterUt}, {gjennomsnittUt}, {len(maalingerUt)}\n ")
+                filUt.write(f"{counterUt}, {gjennomsnittUt}, {len(maalingerUt)}\n ")
                 maalingerUt.clear()
+                sammenlignHoyde(filInn, filUt, counterUt, dato)
     except:
         print("ERROR")
-        print("Sjekk port")
+        print("Sjekk port") # Vanligste feil er portnavn
         exit()
 
 def regnGjennomsnitt(maalinger):
-    #regner ut gjennomsnitt
+    # Regner ut gjennomsnitt
     gjennomsnitt = 0
     for i in range(len(maalinger)):
         gjennomsnitt += float(maalinger[i])
     gjennomsnitt = gjennomsnitt/len(maalinger)
     return gjennomsnitt
 
+def sammenlignHoyde(filInn, filUt, counterUt, dato):
+    # Sammenligner høyden til posen som går ut med høyden til posen som gikk inn
+    # Skriver til fil
+    filInn = open(f"{dato}Inn.csv", "r")
+    filUt = open(f"{dato}Ut.csv", "r")
+    inn = filInn.readlines()
+    ut = filUt.readlines()
+    lekkasje = float(inn[counterUt][1]) - float(ut[counterUt][1])
+    filSammenlign = open(f"{dato}Sammenlign.csv", "a")
+    filSammenlign.write(f"{counterUt}, {[counterUt][1]}, {ut[counterUt][1]}, {lekkasje}\n") 
+    filInn.close()
+    filUt.close()
+    filSammenlign.close()
+
 # Argumentparser, tar inn portnavn
 ap = argparse.ArgumentParser()
 ap.add_argument("-p","--port",required = True, help = "Enter Port Name")
+ap.add_argument("-sz","--size",required = False, help = "Enter Size of the Bag")
 args = vars(ap.parse_args())
 
 PORT = args['port']
@@ -73,3 +93,4 @@ PORT = args['port']
 maalHoyde(PORT)
 
 # lengde potmeter: 5.2 cm
+# sudo python3 lesHoyde.py -p "portnavn" -sz "størrelse"
